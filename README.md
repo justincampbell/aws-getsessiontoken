ebb327cc2cbbf536db164d7477198a7bc416d5af
Justin Campbell <justin@justincampbell.me>
2016-09-28 11:10:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# aws-getsessiontoken
+
+A small utility for testing [AWS STS GetSessionToken](http://docs.aws.amazon.com/STS/latest/APIReference/API_GetSessionToken.html) requests.
+
+## Usage
+
+Run the utility with an IAM Device Serial Number, and enter our MFA Token Code:
+
+```
+$ go run main.go -s "arn:aws:iam::123456789012:mfa/justin"
+MFA Token Code: 123456
+
+Request: {
+  DurationSeconds: 900,
+  SerialNumber: "arn:aws:iam::123456789012:mfa/justin",
+  TokenCode: "123456"
+}
+
+Response: {
+  Credentials: {
+    AccessKeyId: "ASIAIDABCDEFGHIJKLMN",
+    Expiration: 2016-09-28 15:14:37 +0000 UTC,
+    SecretAccessKey: "x2AMZpgAzfnR4KmIlqwcJ2KLYG7sHu9dBH+nQkRxbpM",
+    SessionToken: "GH/nEHnk846JH99JJiXYG4SyxwS18h9e+dNtMCUs9BwpuHut8KkDV9cMYUtu/RBLaFdMpX8lxWkNwH6+LRd5oN0VMGyAhs+1QWHvJdQVwGUoYi/tPU5dRt4m16QUp/9+r3KBmp5pLzQQ43MLgPgVOnIUqzccbr/YysvB0ess2sY="
+  }
+}
+
+export AWS_ACCESS_KEY_ID=ASIAIDABCDEFGHIJKLMN
+export AWS_SECRET_ACCESS_KEY=x2AMZpgAzfnR4KmIlqwcJ2KLYG7sHu9dBH+nQkRxbpM=
+export AWS_SESSION_TOKEN=GH/nEHnk846JH99JJiXYG4SyxwS18h9e+dNtMCUs9BwpuHut8KkDV9cMYUtu/RBLaFdMpX8lxWkNwH6+LRd5oN0VMGyAhs+1QWHvJdQVwGUoYi/tPU5dRt4m16QUp/9+r3KBmp5pLzQQ43MLgPgVOnIUqzccbr/YysvB0ess2sY=
+```
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+)
+
+var (
+	durationSeconds int
+	serialNumber    string
+)
+
+func init() {
+	flag.IntVar(&durationSeconds, "d", 900, "DurationSeconds: The duration, in seconds, that the credentials should remain valid. Acceptable durations for IAM user sessions range from 900 seconds (15 minutes) to 129600 seconds (36 hours), with 43200 seconds (12 hours) as the default. Sessions for AWS account owners are restricted to a maximum of 3600 seconds (one hour). If the duration is longer than one hour, the session for AWS account owners defaults to one hour.")
+	flag.StringVar(&serialNumber, "s", "", "SerialNumber: The identification number of the MFA device that is associated with the IAM user who is making the GetSessionToken call. Specify this value if the IAM user has a policy that requires MFA authentication. The value is either the serial number for a hardware device (such as GAHT12345678) or an Amazon Resource Name (ARN) for a virtual device (such as arn:aws:iam::123456789012:mfa/user). You can find the device for an IAM user by going to the AWS Management Console and viewing the user's security credentials.")
+}
+
+func main() {
+	flag.Parse()
+
+	fmt.Printf("MFA Token Code: ")
+	tokenCode, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	fmt.Printf("\n")
+	if err != nil {
+		panic(err)
+	}
+	tokenCode = strings.TrimSpace(tokenCode)
+
+	service := sts.New(session.New())
+
+	request := &sts.GetSessionTokenInput{
+		DurationSeconds: aws.Int64(int64(durationSeconds)),
+		SerialNumber:    aws.String(serialNumber),
+		TokenCode:       aws.String(tokenCode),
+	}
+	fmt.Printf("Request: %v\n\n", request)
+
+	response, err := service.GetSessionToken(request)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Response: %v\n\n", response)
+
+	fmt.Printf("export AWS_ACCESS_KEY_ID=%s\n", *response.Credentials.AccessKeyId)
+	fmt.Printf("export AWS_SECRET_ACCESS_KEY=%s\n", *response.Credentials.SecretAccessKey)
+	fmt.Printf("export AWS_SESSION_TOKEN=%s\n", *response.Credentials.SessionToken)
+}
